d5986e7f843598c64a943486333a130f476bf1ed
Jacob Sierra <jacobisaac22@yahoo.com>
2021-07-15 12:34:31 -0500

    add links to given routes

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -10,3 +10,12 @@ const PORT = process.env.PORT || 8080;
 // set up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'))
+
+// points to server for route file, routes gives server a map how to respond
+require('./assets/js/index')(app);
+
+// link to given routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
