80dfe0ada062d1bf4f9f97cf3f8951d7a185fec6
Jacob Sierra <jacobisaac22@yahoo.com>
2021-07-15 13:13:02 -0500

    added routes for apis to read json file

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -15,7 +15,40 @@ app.use(express.static('public'))
 // points to server for route file, routes gives server a map how to respond
 require('./assets/js/index')(app);
 
-// link to given routes
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+// link routes for html
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+// link routes for api to read from json file
+app.get('/api/notes', function (req, res) {
+    fs.readFile(__dirname + '/db/db.json', function (err, data) {
+        if (err) {
+          return console.log(err)
+        } else {
+            res.json(JSON.parse(data))
+        }
+    })
+});
+
+app.post('/api/notes', function (req, res ) {
+    fs.readFile(__dirname + '/db/db.json', function (err, data) {
+        if (err) {
+            return console.log(err)
+        } else {
+            notes = JSON.parse(notes)
+            let id = notes[notes.length -1].id + 1
+            let newNote = { title: req.body.title, text: req.body.text, id: id}
+            let activeNote = notes.concat(newNote)
+
+            fs.writeFile(__dirname + 'db/db.json', JSON.stringify(activeNote), function (err, data){
+                if (err){
+                    return error
+                } else{
+                    console.log(activeNote)
+                    res.json(activeNote)
+                }
+            })
+        }
+    })
+});
 
